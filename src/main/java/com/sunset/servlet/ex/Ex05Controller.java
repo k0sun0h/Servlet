e0f478f0517f30186b8e04e76690850814a1e04d
김선혜 <kimsunhye1225@gmail.com>
2025-05-22 15:02:50 +0900

    [김선혜] - post method 일부 예제 작성

--- src/main/java/com/sunset/servlet/ex/Ex05Controller.java
+++ src/main/java/com/sunset/servlet/ex/Ex05Controller.java
@@ -1,13 +1,39 @@
 package com.sunset.servlet.ex;
 
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class Ex05Controller extends HttpServlet {
 
+	// 파라미터가 
+	// 보안에 민감한 데이터가 포함되어 있는지
+	//긴 데이터가 포함되어 있는지 
 	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) {
+		
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		
+		
+		// 이름 생년월일 전달 받고, 이름과 나이를 html로 구성 
+		String name = request.getParameter("name");
+		String birthDay = request.getParameter("birthday");
+		// 20040116
+		String yearString = birthDay.substring(0, 4);
+		int year = Integer.parseInt(yearString);
+		
+		int age = 2022 - year + 1;
+		
+		out.println("<html><head><title>post method</title></head> <body>");
+		
+		out.println("이름 : " + name);
+		out.println("나이 : " + age);
+		
+		out.println("</body></html>");
 		
 	}
 	
